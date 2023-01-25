b830d05c233d27ac6180daee5c5b9e447dcd1d96
jumppiii <jumppiii@live.no>
2023-01-25 13:37:52 +0000

    Add files via upload

--- Contexto.py
+++ Contexto.py
@@ -1,8 +1,9 @@
 import requests
 import urllib3
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-file = open("wordlistnew.txt", "r")
+file = open("./text/wordlist.txt", "r")
 user_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0'}
 
 error1 = str("This word doesn't count, it's too common")
@@ -10,20 +11,24 @@ error2 = str("I don't know this word")
 
 for entry in file:
     num = ''
+    left = ":"
+    right = ","
 
     urls = "https://api.contexto.me/machado/en/game/129/" + entry
     url = urls[:-1]
 
     response = requests.get(url, headers=user_agent, verify=False)
     reply = response.text
-    
+
     if reply.__contains__(error1) or reply.__contains__(error2):
         continue
 
-    left = ":"
-    right = ","
-    result = reply[reply.index(left)+len(left):reply.index(right)]
+    try:
+        result = reply[reply.index(left) + len(left):reply.index(right)]
+
+    except ValueError:
+        print("ValueError caught in:", reply, "Continuing..")
+        continue
 
     if int(result) < 1000:
         print(result, ":: Word was", entry)
-
